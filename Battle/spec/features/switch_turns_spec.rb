fd161770cfe28ac1a2cb02b6308eb88c5f070306
Jazzy <jasmina.gasper@gmail.com>
2016-03-17 11:16:36 +0000

    switch

--- Battle/spec/features/switch_turns_spec.rb
+++ Battle/spec/features/switch_turns_spec.rb
@@ -1,10 +1,16 @@
 require 'spec_helper'
 
 feature 'switch turns' do
+
+  scenario 'shows who the current player is' do
+    sign_in_and_play
+    expect(page).to have_content "It's Player 1's turn!"
+  end
+
   scenario 'switches from player1 to player2' do
     sign_in_and_play
     click_button "Attack!"
     click_button "Switch turns!"
-    expect(page).to have_content "Now it's Player 2's turn!"
+    expect(page).to have_content "It's Player 2's turn!"
   end
 end
