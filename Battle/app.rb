147aad1fedf79c2b5874bbd682422b6d5d52db69
Jazzy <jasmina.gasper@gmail.com>
2016-03-18 17:01:10 +0000

    poison

--- Battle/app.rb
+++ Battle/app.rb
@@ -45,6 +45,24 @@ enable :sessions
     end
   end
 
+  post '/poison' do
+    Attack.new(Game.instance.current_opponent)
+    Attack.begin(Game.instance.current_opponent)
+    if Game.instance.game_over?
+      redirect '/game_over'
+    else
+      redirect '/poisoned_attack'
+    end
+  end
+
+  get '/attack_poison' do
+    erb :attack_poison
+  end
+
+  get '/poisoned_attack' do
+      erb :poisoned_attack
+  end
+
   get '/attack' do
     if Game.instance.current_player.name == 'Computer'
       erb :computer_attack
@@ -65,6 +83,21 @@ enable :sessions
     else
       redirect('/play')
     end
+  end
+
+  post '/switching_when_poisoned' do
+    Game.instance.switch_player
+      Attack.begin(Game.instance.current_opponent)
+      # Attack.poison(Game.instance.current_player)
+      poisoned = Attack.new(Game.instance.current_player).poison_hp
+      Game.instance.current_player.lose_hp(poisoned)
+      if Game.instance.game_over?
+        redirect '/game_over'
+      elsif  poisoned == 0
+          redirect '/attack'
+      else
+        redirect '/attack_poison'
+      end
   end
 
   get '/game_over' do
--- Battle/lib/attack.rb
+++ Battle/lib/attack.rb
@@ -2,6 +2,8 @@ require_relative 'player'
 
 class Attack
 
+	attr_reader :player
+	
 	def initialize(player)
 		@player = player
 	end
@@ -10,9 +12,25 @@ class Attack
 		Attack.new(player).attack
 	end
 
+	def self.poison(player)
+		Attack.new(player).player.lose_hp(poison_hp)
+	end
+
   	def attack
-      	@player.lose_hp(damage)
+      @player.lose_hp(damage)
   	end
+
+		def poison
+			@player.lose_hp(damage * probability)
+		end
+
+		def poison_hp
+			damage * probability
+		end
+
+		def probability
+			Kernel.rand(2)
+		end
 
   	def damage
   		Kernel.rand(10)
